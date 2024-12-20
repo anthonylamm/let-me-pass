7428d1248f2aeb7931acda8d7a9b45c73d754fd8
Anthony lam <alam389@uwo.ca>
2024-12-20 17:02:23 -0500

    added login functionality

--- server/routes/public.js
+++ server/routes/public.js
@@ -12,7 +12,6 @@ router.post('/register', [
     body('email').isEmail().normalizeEmail().withMessage('Invalid email address'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
 ], async (req, res) => {
-    console.log('Received request:', req.body); // Debug log
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         console.log('Validation errors:', errors.array()); // Debug log
@@ -22,14 +21,14 @@ router.post('/register', [
 
     const client = await pool.connect();
     try {
-        const checkUser = await client.query('SELECT * FROM users WHERE email = $1 OR username = $2', [email, username]);
+        const checkUser = await client.query('SELECT * FROM users WHERE email = $1 OR username = $2', [email, username]);//checking to see if user already exists
         if (checkUser.rows.length > 0) {
             return res.status(400).json({ error: 'Email or username already exists' });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);//algorithm to hash password
 
-        const result = await client.query('INSERT INTO users (username, email, password, email_verified) VALUES ($1, $2, $3, $4) RETURNING *', [username, email, hashedPassword, false]);
+        const result = await client.query('INSERT INTO users (username, email, password, email_verified) VALUES ($1, $2, $3, $4) RETURNING *', [username, email, hashedPassword, false]);//inserting user into database
         console.log(result.rows[0]);
 
         const token = jwt.sign({ username: result.rows[0].username }, process.env.JWT_SECRET, { expiresIn: '3h' });
@@ -53,7 +52,8 @@ router.post('/register', [
     }
 }); 
 
-router.get('/verify-email', async (req, res) => {
+router.get('/verify-email', 
+    async (req, res) => {
     const { token } = req.query;
 
     try {
@@ -77,8 +77,44 @@ router.get('/verify-email', async (req, res) => {
     }
 });
 
-router.post('/auth', async (req, res) => {
-    
+router.post('/login', [
+    body('username').trim().escape(),
+    body('password').trim().escape()
+], async (req,res)=> {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {username,password} = req.body;
+    const client = await pool.connect();
+    try {
+       
+
+        console.log(username, password)
+        const results = await client.query('SELECT * FROM users WHERE username =$1', [username]);//getting results from database 
+        if (results.rows.length === 0) { //checking to see if user exists
+            return res.status(400).json({ error: 'Invalid username or password' });
+        }
+        const user = results.rows[0];
+        const passwordMatch = await bcrypt.compare(password, user.password);//unhashing the password and comparing it to the password in the database
+        if (!passwordMatch) {
+            return res.status(400).json({ error: 'Invalid username or password' });
+        }
+        jwt.sign({ username: user.username }, process.env.JWT_SECRET, { expiresIn: '3h' }, (err, token) => {//creating a token to be used for authentication
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Internal server error' });
+            }
+            res.json({ token });
+        });
+       
+    }catch (error) {
+        console.error("Database query error:", error);
+        return res.status(500).json({ error: 'Internal server error.' });
+      } finally {
+        client.release();
+      }
 });
 
 module.exports = router;
